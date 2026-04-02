f087ff8bd2d5223313d410fcbd3a09cce05c312b
Jasmine Jimenez <jj11765252gmail.com>
2026-04-02 11:40:04 +0800

    Add game logic and update scores

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ function play(playerChoice){
     document.getElementById("computerChoice").innerText =
         "Computer chose: " + computerChoice;
 
+        let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "It's a Draw!";
+    }
+    else if(
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ){
+        result = "Player Wins!";
+        playerScore++;
+    }
+    else{
+        result = "Computer Wins!";
+        computerScore++;
+    }
+
+    document.getElementById("result").innerText = result;
+    document.getElementById("playerScore").innerText = playerScore;
+    document.getElementById("computerScore").innerText = computerScore;
+
 }
